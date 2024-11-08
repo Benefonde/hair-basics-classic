a73f7d5fe98b249d9c710256e605f5f324148935
Benefond <80904546+Benefonde@users.noreply.github.com>
2024-11-08 07:31:25 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    heklp me

--- Assets/Scripts/PickupScript.cs
+++ Assets/Scripts/PickupScript.cs
@@ -23,59 +23,7 @@ public class PickupScript : MonoBehaviour
             {
                 if (raycastHit.transform.gameObject == transform.gameObject & Vector3.Distance(this.player.position, base.transform.position) < 10f & Cursor.lockState == CursorLockMode.Locked)
                 {
-                    if (transform.name == "Pickup_Slab")
-                    {
-                        if (gc.mode != "story")
-                        {
-                            gameObject.SetActive(false);
-                        }
-                        if (this.gc.item[0] == 0 | this.gc.item[1] == 0 | this.gc.item[2] == 0 || this.gc.item[3] == 0)
-                        {
-                            if (((ID == 25 && gc.item[gc.itemSelected] == 25) || (ID != 25)) && GetComponentInChildren<SpriteRenderer>().sprite.texture != gc.itemTextures[0] && gc.item[gc.itemSelected] != 0)
-                            {
-                                gc.CurseOfRa();
-                                Texture itemTexture = gc.itemTextures[0];
-                                Sprite itemSprite = Sprite.Create((Texture2D)itemTexture, new Rect(0, 0, itemTexture.width, itemTexture.height), new Vector2(0.5f, 0.5f), itemTexture.width * 1.55f);
-                                GetComponentInChildren<SpriteRenderer>().sprite = itemSprite;
-                                return;
-                            }
-                            if (GetComponentInChildren<SpriteRenderer>().sprite.texture == gc.itemTextures[0] && gc.item[gc.itemSelected] != 0)
-                            {
-                                ID = gc.item[gc.itemSelected];
-                                Texture itemTexture = gc.itemTextures[ID];
-                                Sprite itemSprite = Sprite.Create((Texture2D)itemTexture, new Rect(0, 0, itemTexture.width, itemTexture.height), new Vector2(0.5f, 0.5f), itemTexture.width * 1.55f);
-                                GetComponentInChildren<SpriteRenderer>().sprite = itemSprite;
-                                gc.ResetItem();
-                                if (ID == 25)
-                                {
-                                    gc.UndoCurse();
-                                }
-                                else
-                                {
-                                    gc.CurseOfRa();
-                                }
-                                return;
-                            }
-                        }
-                        else
-                        {
-                            int orgID = ID;
-                            ID = gc.item[gc.itemSelected];
-                            Texture itemTexture = gc.itemTextures[ID];
-                            Sprite itemSprite = Sprite.Create((Texture2D)itemTexture, new Rect(0, 0, itemTexture.width, itemTexture.height), new Vector2(0.5f, 0.5f), itemTexture.width * 1.55f);
-                            GetComponentInChildren<SpriteRenderer>().sprite = itemSprite;
-                            gc.CollectItem(orgID);
-                            if (ID == 25 && gc.item[gc.itemSelected] != 0)
-                            {
-                                gc.UndoCurse();
-                            }
-                            else if (ID != 25)
-                            {
-                                gc.CurseOfRa();
-                            }
-                        }
-                    }
-                    else if (!zombie)
+                    if (!zombie)
                     {
                         if (this.gc.item[0] == 0 | this.gc.item[1] == 0 | this.gc.item[2] == 0 || this.gc.item[3] == 0)
                         {
