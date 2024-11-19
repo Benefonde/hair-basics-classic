aa57b647ef2d629b03b69672d2349b7f2763b9b3
Benefond <80904546+Benefonde@users.noreply.github.com>
2024-11-19 21:35:33 +0100

    Revert "paninis exclusive demo!"
    This reverts commit 6233307c8c34e754e59f8f902b88168dca82a0c7.

--- Assets/Scripts/VersionScript.cs
+++ Assets/Scripts/VersionScript.cs
@@ -5,12 +5,10 @@ public class VersionScript : MonoBehaviour
 {
     void Start()
     {
-        //GetComponent<TMP_Text>().text = Application.version;
-        GetComponent<TMP_Text>().text = "Panino Exclusive Demo";
+        GetComponent<TMP_Text>().text = Application.version;
         if ((System.DateTime.Now.Month == 10 && System.DateTime.Now.Day >= 20) || (System.DateTime.Now.Month == 11 && System.DateTime.Now.Day <= 7))
         {
-            //GetComponent<TMP_Text>().text = $"EVIL {GetComponent<TMP_Text>().text}";
-            GetComponent<TMP_Text>().text = "EVIL Panino Exclusive Demo";
+            GetComponent<TMP_Text>().text = $"EVIL {GetComponent<TMP_Text>().text}";
         }
     }
 }
