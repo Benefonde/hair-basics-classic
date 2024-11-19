6233307c8c34e754e59f8f902b88168dca82a0c7
Benefond <80904546+Benefonde@users.noreply.github.com>
2024-11-19 21:31:18 +0100

    paninis exclusive demo!

--- Assets/Scripts/VersionScript.cs
+++ Assets/Scripts/VersionScript.cs
@@ -5,10 +5,12 @@ public class VersionScript : MonoBehaviour
 {
     void Start()
     {
-        GetComponent<TMP_Text>().text = Application.version;
+        //GetComponent<TMP_Text>().text = Application.version;
+        GetComponent<TMP_Text>().text = "Panino Exclusive Demo";
         if ((System.DateTime.Now.Month == 10 && System.DateTime.Now.Day >= 20) || (System.DateTime.Now.Month == 11 && System.DateTime.Now.Day <= 7))
         {
-            GetComponent<TMP_Text>().text = $"EVIL {GetComponent<TMP_Text>().text}";
+            //GetComponent<TMP_Text>().text = $"EVIL {GetComponent<TMP_Text>().text}";
+            GetComponent<TMP_Text>().text = "EVIL Panino Exclusive Demo";
         }
     }
 }
