f238057f87b5ba03d6f6cbf90ac2c0e0cd4916ab
Benefond <80904546+Benefonde@users.noreply.github.com>
2025-03-16 17:56:16 +0100

    april fools build coming up [on april 1st]

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -7,7 +7,7 @@ public class TitleScreen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (System.DateTime.Now.Month == 4 && System.DateTime.Now.Day == 1)
+        //if (System.DateTime.Now.Month == 4 && System.DateTime.Now.Day == 1)
         {
             primaaprilis.SetActive(true);
         }
